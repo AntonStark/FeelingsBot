7abf0e6f03d6586147e7a0f5002418039f70fac2
Anton Iakovlev <lantar23@yandex.ru>
2023-06-03 12:42:58 +0300

    add logging

--- feelings_bot/utils/times.py
+++ feelings_bot/utils/times.py
@@ -3,8 +3,9 @@ from datetime import datetime
 
 
 def random_datetime_today(hour_min: int, hour_max: int) -> datetime:
+    # -3 because of timezones
     datetime_ = datetime.today().replace(
-        hour=int(random.triangular(hour_min, hour_max)) - 3,  # -3 because of timezones
+        hour=int(random.triangular(hour_min, hour_max)) - 3,
         minute=random.randint(0, 59),
     )
     return datetime_
